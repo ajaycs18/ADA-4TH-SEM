4005c868b3b07dc09db68825cfc7c38936881a80
ajaycs18 <ajay.cs18+lab@bmsce.ac.in>
2020-01-30 11:53:30 +0530

    Add files via upload

---
+++ binsearch.py
@@ -0,0 +1,20 @@
+testcases = int(input())
+
+for _ in range(testcases):
+    n, ele = map(int, input().split(" ")) 
+    arr = list(map(int, input().split(" ")))
+
+    start, end = 0, len(arr) 
+    while start <= end:
+        mid = (start + end) // 2
+        if arr[mid] == ele:
+           print(1)
+           break;
+        elif arr[mid] > ele:
+           end = mid - 1
+        else:
+            start = mid + 1
+
+    if start > end:
+        print(-1)
+
