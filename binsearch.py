6a176907e2ef98a75c4b35e7d598bfa8fd1ec0cc
ajaycs18 <ajay.cs18+lab@bmsce.ac.in>
2020-01-30 12:00:21 +0530

    added sorting

--- binsearch.py
+++ binsearch.py
@@ -4,6 +4,9 @@ for _ in range(testcases):
     n, ele = map(int, input().split(" ")) 
     arr = list(map(int, input().split(" ")))
 
+    # sort if required
+    arr = sorted(arr)
+    
     start, end = 0, len(arr) 
     while start <= end:
         mid = (start + end) // 2
