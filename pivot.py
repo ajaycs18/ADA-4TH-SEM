eaf6969fc791ce9e4ed260b071f72232bc0c9bcb
ajaycs18 <ajay.cs18+lab@bmsce.ac.in>
2020-02-20 21:21:58 +0530

    Update pivot.py

--- pivot.py
+++ pivot.py
@@ -1,18 +1,37 @@
 def findPivot(arr, low, high): 
-    if high < low: 
-        return -1
-    if high == low: 
-        return low 
-      
-    mid = int((low + high)/2) 
-      
-    if mid < high and arr[mid] > arr[mid + 1]: 
-        return mid 
-    if mid > low and arr[mid] < arr[mid - 1]: 
-        return (mid-1) 
-    if arr[low] >= arr[mid]: 
-        return findPivot(arr, low, mid-1) 
-    return findPivot(arr, mid + 1, high) 
+  if high < low: 
+    return -1
+  if high == low: 
+    return low 
+  mid = int((low + high)/2) 
+  if mid < high and arr[mid] > arr[mid + 1]: 
+    return mid 
+  if mid > low and arr[mid] < arr[mid - 1]: 
+    return (mid-1) 
+  if arr[low] >= arr[mid]: 
+    return findPivot(arr, low, mid-1) 
+  return findPivot(arr, mid + 1, high) 
+
+def binarySearch(arr, low, high, key): 
+  if high < low: 
+    print("Not found")	 
+  mid = int((low + high)/2) 
+  if key == arr[mid]: 
+    print("found at", mid)
+    return
+  if key > arr[mid]: 
+    return binarySearch(arr, (mid + 1), high, key) 
+  return binarySearch(arr, low, (mid -1), key); 
 
 arr = list(map(int, input().split()))
-print(arr[findPivot(arr, 0, len(arr) - 1)])
+pivot = findPivot(arr, 0, len(arr) - 1)
+print("pivot ele:", arr[pivot])
+ele = int(input("enter ele to find: "))
+
+if pivot == -1: 
+  binarySearch(arr, 0, len(arr)-1, ele)
+if arr[pivot] == ele: 
+  print("Found at", pivot)
+if arr[0] <= ele: 
+  binarySearch(arr, 0, pivot-1, ele) 
+binarySearch(arr, pivot+1,len(arr)-1, ele)
